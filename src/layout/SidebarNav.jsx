6244d74732476649e794630fc1a4d8f1a167a115
rlaeksxp3457 <rlaeksxp3457@gmail.com>
2023-06-15 02:41:00 +0900

    사이드바 핸들링 이벤트 추가

--- src/layout/SidebarNav.jsx
+++ src/layout/SidebarNav.jsx
@@ -1,6 +1,13 @@
 import { pageData } from '../data';
+import { useNavigate } from 'react-router-dom'
 
 function SidebarNav(props) {
+	let navigate = useNavigate();
+	const handleSidebar = (item)=> ()=>{
+		props.setSidebar(false)
+		navigate(`/${item.url}`)
+	}
+
 	return (
 		<>
 			<div className={`sidebar ${ props.sidebar === true ? "active" : "" }`}>
@@ -9,24 +16,22 @@ function SidebarNav(props) {
 						<i className="fas fa-times" />
 					</div>
 					<div className="category flex">
-					<img
-						className="category_icon"
-						src="../img/icons/location_icon_green.png"
-						alt="길찾기 아이콘"
-					/>
-					<p>길찾기</p>
+						<img
+							className="category_icon"
+							src="../img/icons/location_icon_green.png"
+							alt="길찾기 아이콘"
+						/>
+						<p>길찾기</p>
 					</div>
 					{
 						pageData.map((item,idx)=>(
-							<a href={`/${item.url}`} key={idx}>
-								<div className="menu_item">
-									<p className="flex">
+							<div className="menu_item" onClick={handleSidebar(item)} key={idx}>
+								<p className="flex">
 									<span className="square" />
 									<span className="title">{item.title}</span>
-									</p>
-									<p className="kr_desc">{item.desc}</p>
-								</div>
-							</a>
+								</p>
+								<p className="kr_desc">{item.desc}</p>
+							</div>
 						))
 					}
 				</div>
